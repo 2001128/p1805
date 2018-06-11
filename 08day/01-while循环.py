9a1fe9169f9ad6621a8b558e5642dba3ccf703bf
dongshuai2001 <335775879@qq.com>
2018-06-11 18:49:06 +0800

    08day/

--- 08day/01-while循环.py
+++ 08day/01-while循环.py
@@ -3,11 +3,11 @@ while i < 10:
     print("向往明天")
     i+=1
 '''
-asum = 0
 i = 0
- while i < 100:
+asum = 0
+while i < 100:
     print("当前数字:%d"%i)
     i+=1    
-    i = i +a
+    asum = asum+i
 print("和为:%d"%asum)
 
